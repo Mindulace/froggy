9f810d906fa4f4f3ed26797a1ac0a5c3c0b40c2b
Hendrik <HBonke@gmx.net>
2019-03-07 20:55:32 +0100

    add option to config

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Webhook:         This is the URL from the discord server.
+ * Random_message:  To activate random messages.
+ *      false   =>  The bot is using the first message in array.
+ *      true    =>  The bot is now choosing a random message.
  * Message:         This is the message, which the bot will be posting.
  * 
  * ExplicitUser:    This is the option for mentioning specific users.
@@ -10,10 +13,20 @@
 
 return array(
     'discord' => array(
-        'webhook' => 'https://discordapp.com/api/webhooks/EXAMPLE',
-        'message' => 'Quaaak.. Denkt an euren :frog: of the day! @everyone',
+        'webhook'           => 'https://discordapp.com/api/webhooks/EXAMPLE',
+        'random_message'    => true,
+        'messages'          => array(
+            'Quaaak.. Denkt an euren :frog: of the day! @everyone',
+            'Hello, ... it\'s me. Can you hear me? Hellooo from the :frog: siiiiide! @everyone',
+            'Guten Tag, dies ist Ihre automatische :frog: - Reminder. Bitte hinterlassen Sie Ihren :frog: nach dem Signalton. Piep! @everyone',
+            'What does the :frog: say? Ring-ding-ding-ding-dingeringeding! @everyone',
+            'A :frog: a day, keeps your Sorrows away! @everyone',
+            'Alarm, Alarm! .... :frog: ! @everyone',
+            'Es spricht die automatische :frog: - Reminder. Bitte tragen Sie Ihren :frog: ein! @everyone',
+            ':frog: - Alarm! Bei Risiken und Nebenwirkungen fragen Sie Ihren :frog: oder Apotheker! @everyone'
+        ),
     ),
     'general' => array(
-        'explicitUser' => false, // Disabled for the first version
+        'explicitUser'      => false, // Disabled for the first version
     )
 );
