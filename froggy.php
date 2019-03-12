f608dee978f41ee3f239849e40b1123a52c7314d
Hendrik <HBonke@gmx.net>
2019-03-12 12:46:41 +0100

    add exception

--- froggy.php
+++ froggy.php
@@ -32,7 +32,7 @@ class froggy
     {
         $ch = curl_init($this->config['discord']['webhook']);
     
-        if($this->config['discord']['random_message']) {
+        if ($this->config['discord']['random_message']) {
             $message = $this->config['discord']['messages'][array_rand($this->config['discord']['messages'])];
         } else {
             $message = $this->config['discord']['messages'][0];
@@ -46,12 +46,22 @@ class froggy
     
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
     
-        $result = curl_exec($ch);
-    
-        echo ("Message sent.");
-    }
+        try {
+            $resultEncoded = curl_exec($ch);
+        } catch (\Exception $e) {
+            throw new \Exception('Cannot send Message');
+        }
 
-    
+        $resultDecoded = (array)json_decode($resultEncoded);
+
+        if ($resultEncoded) {
+            $resultDecoded = (array)json_decode($resultEncoded);
+            echo ('Couldn\'t send message, Server returned errorcode <b>' . $resultDecoded['code'] . '</b> with the message <b>' . $resultDecoded['message'] .'</b>.');
+        } else {
+            echo ("Message sent.");
+        }
+    }
 }
